e7f68d2e5654b8948494ccf63946d659c1a2039b
teddysmithdev <teddysmithdev@gmail.com>
2020-03-02 11:24:42 -0500

    Total Refactor

---
+++ ssh/tasks.py
@@ -0,0 +1,46 @@
+from __future__ import absolute_import, unicode_literals
+import random
+import time
+import socket
+import os
+from .models import ConnectionItem
+from celery.decorators import task
+
+os.environ.setdefault('FORKED_BY_MULTIPROCESSING', '1')
+
+
+
+@task(name="runssh")
+def main():
+	print('Starting honeypot!')
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.bind(('0.0.0.0', 22))
+	s.listen(100)
+	while True:
+		(insock, address) = s.accept()
+		print('Connection from: %s:%d' % (address[0], address[1]))
+		try:
+			insock.send('SSH login: '.encode())
+			data = insock.recv(1024)
+			log = ConnectionItem(address=address[0], port=address[1], data=data)
+			log.save()
+			insock.close()
+		except socket.error as e:
+			print('aaaaaaa')
+		else:
+			print('bbbbbbb')
+
+
+
+@task(name="sum_two_numbers")
+def add(x, y):
+	return x + y
+
+@task(name="multiply_two_numbers")
+def mul(x, y):
+	total = x * (y * random.randint(3, 100))
+	return total
+
+@task(name="sum_list_numbers")
+def xsum(numbers):
+	return sum(numbers)
